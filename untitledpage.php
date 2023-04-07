df5e60fc089a028da649c667a69db0e95106c7ad
radim776 <130166108+radim776@users.noreply.github.com>
2023-04-07 17:19:18 +0200

    Add files via upload

---
+++ untitledpage.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="generator" content="RocketCake">
+	<title></title>
+	<style type="text/css">
+		a { color:#5D5D9E; } 
+		a:visited { color:#5D5D9E; } 
+		a:active { color:#5C615E; } 
+		a:hover { color:#B2B3B4; } 
+	</style>
+</head>
+<body style="background-color:#000000; padding:0;  margin: 0;">
+<div style="text-align:left;">
+<video  style="box-sizing: border-box; vertical-align: bottom; position:relative; display: inline-block; width:73%; background-color:#D1D1D1; border: 1px solid #A0A0A0; " id="video_6df3d864" src="rc_images/2023_04_06_18_59_38__online_video_cutter_com_.mp4" autoplay="autoplay"  loop="true"  poster="rc_images/load_win.png">
+Your browser does not support the video element.
+</video>  </div>
+</body>
+</html>
